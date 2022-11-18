e940b572fc0cc0cd4a2c9f932a41757e044227f2
Altanim <altanim0404@gmail.com>
2022-11-18 14:44:49 +0800

    Выполнил задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 
         public class Main {
             public static void main(String[] args) {
+
+// Задание №1
+                System.out.println("Задание №1");
+                String firstName = "Ivan ";
+                String middleName = "Ivanovich ";
+                String lastName = "Ivanov ";
+                String fullName = lastName + firstName + middleName;
+                System.out.println("ФИО сотрудника - " + fullName);
+
+                // Задание №2
+                System.out.println("\nЗадание №2");
+                String upFullName = fullName.toUpperCase();
+                System.out.println(upFullName);
+
+                // Задание №3
+                System.out.println("\nЗадание №3");
+                String fullName1 = "Иванов Семён Семёнович";
+                if (fullName1.contains("ё")) {
+                    fullName1 = fullName1.replace('ё', 'е');
+                }
+                System.out.println(fullName1);
             }
             }
